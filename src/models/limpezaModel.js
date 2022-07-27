5a218278af44d3a51d80c3c2112e94d3c21dd631
letxns <lele_martins1999@yahoo.com.br>
2022-07-27 00:04:10 -0300

    feat: cria arquivo model contendo a classe referente à entidade limpeza

---
+++ src/models/limpezaModel.js
@@ -0,0 +1,10 @@
+class LimpezaModel {
+    constructor (id, numeroQuarto, controle, id_funcionario){
+        this.id = id
+        this.numeroQuarto = numeroQuarto
+        this.controle = controle
+        this.id_funcionario = id_funcionario
+    }
+}
+
+export default LimpezaModel
