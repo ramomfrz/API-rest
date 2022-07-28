2f455b7748e4cc7bf2b6026255ba1911d973f436
Ramom Ferraz <ramomferraz@hotmail.com>
2022-07-28 17:55:06 -0300

    Build: Cria arquivo Reservas que contém um constructor.

---
+++ src/models/ReservasModel.js
@@ -0,0 +1,10 @@
+class ModelReservas {
+    constructor(CPF_Cliente, NumeroQuarto, data_entrada, data_saida) {
+        this.CPF_Cliente = CPF_Cliente
+        this.NumeroQuarto = NumeroQuarto
+        this.data_entrada = data_entrada
+        this.data_saida = data_saida
+    }
+}
+
+export default ModelReservas
