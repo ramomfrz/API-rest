01f77c42a1081961e831aa12aa239e1e3fa5a9d5
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2022-07-29 16:16:52 -0300

    feat: cria a modelagem da entidade

---
+++ src/models/QuartosModel.js
@@ -0,0 +1,10 @@
+class QuartosModel{
+    constructor(numero, nome, controleLimpeza, telefone){
+        this.numero = numero
+        this.nome = nome
+        this.controleLimpeza = controleLimpeza
+        this.telefone = telefone
+    }
+};
+
+export default QuartosModel;
