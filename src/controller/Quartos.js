ca5aa05a3933eb07d7a82dfaf2066f3d0118d1a8
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2022-07-29 15:33:03 -0300

    feat: cria as rotas GET/POST/PUT/DELETE

---
+++ src/controller/Quartos.js
@@ -0,0 +1,70 @@
+import DatabaseMetodos from "../DAO/DatabaseMetodos.js";
+import QuartosModel from "../models/QuartosModel.js";
+import ValidacoesQuartos from "../services/ValidacoesQuartos.js";
+
+class Quartos{
+    static rotas(app){
+        
+        app.get("/quartos", async (req, res) => {
+            const response = await DatabaseMetodos.listarQuartos()
+            res.status(200).json(response)
+        })
+
+        app.get("/quartos/:id", async (req, res) => {
+            try{
+                const quarto = await DatabaseMetodos.listarQuarto(req.params.id);
+                if(!quarto){
+                    throw new Error("Não foi encontrado um quarto com esse ID")
+                }
+                res.status(200).json(quarto)
+            }catch(error){
+                res.status(404).json(error.message)
+            }
+        })
+
+
+        app.post("/quartos", async (req, res) => {
+            const validaGeral =  ValidacoesQuartos.validaGeral(...Object.values(req.body));
+            try{
+                if(!validaGeral){
+                    const quarto = new QuartosModel(...Object.values(req.body));
+                    const response = await DatabaseMetodos.inserirQuarto(quarto);
+                    res.status(201).json(response)
+                }else{
+                    throw new Error("A requisição está incompleta, revise o corpo")
+                }
+            }catch(error){
+                res.status(400).json(error.message)
+            }
+        });
+
+        app.put("/quartos/:id", async (req, res) => {
+            const validaGeral =  ValidacoesQuartos.validaGeral(...Object.values(req.body));
+            try{
+                if(!validaGeral){
+                    const quarto = new QuartosModel(...Object.values(req.body));
+                    const response = await DatabaseMetodos.atualizarQuarto(req.params.id, quarto);
+                    res.status(201).json(response)
+                }else{
+                    throw new Error("Falha ao atualizar o quarto")
+                }
+            }catch(error){
+                res.status(400).json(error.message)
+            }
+        });
+
+        app.delete("/quartos/:id", async (req, res) => {
+            try{
+                const quarto = await DatabaseMetodos.deletarQuarto(req.params.id);
+                if(!quarto){
+                    throw new Error("Não foi encontrado um quarto com esse ID")
+                }
+                res.status(200).json(quarto)
+            }catch(error){
+                res.status(400).json(error.message)
+            }
+        });
+    }
+}
+
+export default Quartos;
