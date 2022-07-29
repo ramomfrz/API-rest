d05e940c6d13477270e1cb65706bc5c09a1ddcc4
Ramom Ferraz <ramomferraz@hotmail.com>
2022-07-29 20:45:38 -0300

    Adiciona Objeto de reservas para a table do hotel

--- src/database/cria-e-popula.js
+++ src/database/cria-e-popula.js
@@ -7,18 +7,28 @@ const limpeza = {
     id_funcionario: "2"
 }
 
-const reservas = {
-    cpfCliente: "12345678912"
+const reserva = {
+    cpfCliente: "12345678912",
+    numeroQuarto: 4,
+    dataEntrada: "01/01/22",
+    dataSaida: "10/01/22"
 }
 
 try {
 
     await DAO.ativaForeignKeys()
+
     const limpezas = await DatabaseLimpezasMetodos.createTableLimpezas()
     console.log(limpezas, "Limpezas")
 
+    const reservas = await DatabaseReservasMetodos.createTableReservas()
+    console.log(reservas, "Criada Reservas")
+
     const criouLimpeza = await DatabaseLimpezasMetodos.inserirLimpeza(limpeza)
     console.log(criouLimpeza)
+
+    const reservaRegistrada = await DatabaseReservasMetodos.inserirReserva(reserva)
+    console.log(reservaRegistrada, "Reserva registrada com sucesso.")
 
 } catch (error) {
     console.log("Error: ", error)
