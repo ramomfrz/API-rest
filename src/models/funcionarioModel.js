676b4e4932415ee0724f79df67d001fec0c59a3c
carolinadesouzasilva <carolsouza21@gmail.com>
2022-07-29 16:46:28 -0300

    build: Cria arquivo funcionarioModel na pasta models

---
+++ src/models/funcionarioModel.js
@@ -0,0 +1,9 @@
+class FuncionarioModel {
+    constructor(id, nome, data_admissao){
+        this.id = id
+        this.nome = nome
+        this.data_admissao = data_admissao
+    }
+}
+
+export default FuncionarioModel
