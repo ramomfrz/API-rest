e0c2792a79017971ea758d387c3b1a289f2610b5
Ramom Ferraz <ramomferraz@hotmail.com>
2022-07-30 04:48:16 -0300

    Corrige problemas de rotas no CRUD.

--- src/controller/Reservas.js
+++ src/controller/Reservas.js
@@ -1,29 +1,35 @@
 import ReservasModel from "../models/ReservasModel.js";
-import ValidaReservas from "../services/ValidaReservas.js";
+import ValidacoesService from "../services/ValidacoesService.js";
+import DatabaseReservasMetodos from "../DAO/DatabaseReservasMetodos.js";
 import Database from "../database/database.js"
+
+DatabaseReservasMetodos.createTableReservas()
 
 class Reservas {
     static rotas(app) {
         app.get("/reservas", async (req, res) => {
             const resposta = await DatabaseReservasMetodos.listarReservas()
-            res.status(200).json()
+            res.status(200).json(resposta)
         })
 
-        app.get("/reservas/:cpf", async (req, res) => {
+        app.get("/reservas/:id", async (req, res) => {
             try {
-                const reserva = await DatabaseReservasMetodos.listarReservaPorCpf(req.params.cpf)
+                const reserva = await DatabaseReservasMetodos.listarReservasPorID(req.params.id)
+
                 if (reserva) {
+                    res.status(200).json(reserva)
+                } else {
                     throw new Error("Reserva não encontrada para esse CPF")
-                } res.status(200).json(reserva)
+                }
             } catch (error) {
                 res.status(404).json(error.message)
             }
         })
 
         app.post("/reservas", async (req, res) => {
-            const valido = ValidaReservas.validaCPF(...Object.values(req.body))
+            const reservaValidada = ValidacoesService.reservaValidada(...Object.values(req.body))
             try {
-                if (valido) {
+                if (reservaValidada) {
                     const reserva = new ReservasModel(...Object.values(req.body))
                     const response = await DatabaseReservasMetodos.inserirReserva(reserva)
 
@@ -36,29 +42,36 @@ class Reservas {
             }
         })
 
-        app.put("/reservas/:cpf", (req, res) => {
-            const valido = ValidaReservas.validaCPF(...Object.values(req.body))
-            if (valido) {
-                const reserva = new ReservasModel(...Object.values(req.body))
-                const response = DatabaseReservasMetodos.atualizarReserva(req.params.cpf, reserva)
+        app.put("/reservas/:id", (req, res) => {
+            try {
+                const reservaValidada = ValidacoesService.reservaValidada(...Object.values(req.body))
 
-                res.status(201).json(response)
-            } else {
+                if (reservaValidada) {
+                    const reserva = new ReservasModel(...Object.values(req.body))
+                    const response = DatabaseReservasMetodos.atualizarReserva(req.params.id, reserva)
+
+                    res.status(201).json(response)
+                } else {
+                    throw new Error("Número de reserva inválido.")
+                }
+            } catch (error) {
                 res.status(400).json({ Erro: "Erro" })
             }
         })
 
-        app.patch("/reservas/:cpf", (req, res) => {
-            const response = DatabaseReservasMetodos.atualizaReserva(req.params.cpf, req.body)
+        app.patch("/reservas/:id", (req, res) => {
+            const response = DatabaseReservasMetodos.atualizaReserva(req.params.id, req.body)
             res.status(200).json(response)
         })
-        app.delete("/reservas/:cpf", async (req, res) => {
+
+        app.delete("/reservas/:id", async (req, res) => {
             try {
-                const reserva = await DatabaseReservasMetodos.deletaReserva(req.params.cpf)
+                const reserva = await DatabaseReservasMetodos.excluirReserva(req.params.id)
                 if (reserva) {
-                    throw new Error("Reserva não encontrada, confira o CPF")
+                    res.status(200).json(reserva)
+                } else {
+                    throw new Error("Reserva não encontrada para esse CPF")
                 }
-                res.status(200).json(reserva)
             } catch (error) {
                 res.status(404).json({ Error: error.message })
             }
