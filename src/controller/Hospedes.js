9c53808a515cecfef4181f6a0459e5ed4f64a367
allanbarbozag <allpatrick.b@gmail.com>
2022-07-30 19:15:23 -0300

    feat: cria a rota de put para /hospedes/:cpf, para tornar possível a atualização das informações do hospede.

--- src/controller/Hospedes.js
+++ src/controller/Hospedes.js
@@ -1,46 +1,67 @@
-import DatabaseHospedesMetodos from "../DAO/DatabaseHospedesMetodos.js"
-import ValidacoesService from "../services/ValidacoesService.js"
-import HospedeModel from "../models/HospedeModel.js"
-import Database from "../database/Database.js"
+import DatabaseHospedesMetodos from "../DAO/DatabaseHospedesMetodos.js";
+import ValidacoesService from "../services/ValidacoesService.js";
+import HospedeModel from "../models/HospedeModel.js";
+import Database from "../database/Database.js";
 
-DatabaseHospedesMetodos.createTableHospedes()
+DatabaseHospedesMetodos.createTableHospedes();
 
 class Hospedes {
   static rotas(app) {
     app.get("/hospedes", async (req, res) => {
-      const response = await DatabaseHospedesMetodos.listarHospedes()
-      res.status(200).json(response)
+      const response = await DatabaseHospedesMetodos.listarHospedes();
+      res.status(200).json(response);
       
     })
 
     app.get("/hospedes/:cpf", async (req, res) => {
       try {
-        const hospede = await DatabaseHospedesMetodos.listarHospedePorCPF(req.params.cpf)
+        const hospede = await DatabaseHospedesMetodos.listarHospedePorCPF(req.params.cpf);
+        //console.log(hospede)
         if (!hospede) {
-          throw new Error("Hospede não encontrado. Verifique o CPF informado.")
+          throw new Error("Hospede não encontrado. Verifique o CPF informado.");
         }
-        res.status(200).json(hospede)
+        res.status(200).json(hospede);
       } catch (error) {
-        res.status(404).json(error.message)
+        res.status(404).json(error.message);
       }
     })
 
     app.post("/hospedes", async (req, res) => {
-      const validaHospede = ValidacoesService.validaHospede(...Object.values(req.body))
+      const validaHospede = ValidacoesService.validaHospede(...Object.values(req.body));
       console.log(validaHospede)
 
       try {
         if (validaHospede) {
-          const hospede = new HospedeModel(...Object.values(req.body))
-          const response = await (DatabaseHospedesMetodos.registrarHospede(hospede))
-          res.status(201).json(response)
+          const hospede = new HospedeModel(...Object.values(req.body));
+          const response = await (DatabaseHospedesMetodos.registrarHospede(hospede));
+          res.status(201).json(response);
         } else {
-          throw new Error("Verifique se os dados da requisição estão corretos.")
+          throw new Error("Verifique se os dados da requisição estão corretos.");
         }
       } catch (error) {
-        res.status(400).json(error.message)
+        res.status(400).json(error.message);
       }
     })   
+
+    app.put("/hospedes/:cpf", (req, res) => {
+      const validaHospede = ValidacoesService.validaHospede(...Object.values(req.body));
+      console.log(validaHospede)
+
+      if (validaHospede) {
+        const hospede = new HospedeModel(...Object.values(req.body));
+        
+        const response = DatabaseHospedesMetodos.atualizarHospedesPorCPF(req.params.cpf, hospede)
+        
+        res.status(201).json(response)
+      } else {
+        // const hospede = new HospedeModel(...Object.values(req.body));
+        // console.log(hospede)
+        
+        res.status(400).json({Erro: "Verifique os dados da requisição. Lembre-se, o cpf não pode ser alterado(por enquanto)."})
+      }
+
+
+    })
 
    
   }  
