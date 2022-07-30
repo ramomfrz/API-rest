a5a06c0d1257003a18d9015a197cb967a96b9851
allanbarbozag <allpatrick.b@gmail.com>
2022-07-30 14:35:43 -0300

    feat: cria a rota get para buscar usuários através do CPF informado. Já com o tratamento de erros caso o CPF não conste no banco de dados.

--- src/controller/Hospedes.js
+++ src/controller/Hospedes.js
@@ -2,6 +2,7 @@ import DatabaseHospedesMetodos from "../DAO/DatabaseHospedesMetodos.js"
 import ValidacoesService from "../services/ValidacoesService.js"
 import Database from "../database/Database.js"
 
+
 DatabaseHospedesMetodos.createTableHospedes()
 
 class Hospedes {
@@ -12,9 +13,19 @@ class Hospedes {
       
     })
 
-    // app.get("/hospedes/:cpf", (req, res) => {
+    app.get("/hospedes/:cpf", async (req, res) => {
+      try {
+        const hospede = await DatabaseHospedesMetodos.listarHospedePorCPF(req.params.cpf)
+        if (!hospede) {
+          throw new Error("Hospede não encontrado. Verifique o CPF informado.")
+        }
+        res.status(200).json(hospede)
+      } catch (error) {
+        res.status(404).json(error.message)
+      }
+    })
 
-    // })
+   
   }
 
   
