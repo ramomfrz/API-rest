8f5c1d4a9ed663884aee3c9cace72bd80c7e8e27
letxns <lele_martins1999@yahoo.com.br>
2022-07-30 18:53:19 -0300

    build: funções de atualização e deleção do objeto

--- src/DAO/DAO.js
+++ src/DAO/DAO.js
@@ -7,6 +7,7 @@ class DAO {
         const query = 'PRAGMA foreign_keys = ON'
 
         Database.run(query, error => {
+
             if (error) {
                 console.log(error)
             } else {
@@ -18,6 +19,7 @@ class DAO {
     static createTable(query) {
 
         return new Promise((resolve, reject) => {
+
             Database.run(query, (error) => {
                 if (error) {
                     reject(error.message)
@@ -29,12 +31,15 @@ class DAO {
     }
 
     static inserir(entidade, query) {
+
         const body = Object.values(entidade)
 
         return new Promise((resolve, reject) => {
+
             Database.run(query, [...body], (error) => {
                 if (error) {
-                    reject(error.message)
+                    // reject(error.message)
+                    throw new Error("Cadastro mal sucedido")
                 } else {
                     resolve({ error: false, message: "Cadastrou" })
                 }
@@ -44,27 +49,62 @@ class DAO {
 
 
     static listarTodas(query) {
+
         return new Promise((resolve, reject) => {
-            Database.all(query, (error, resultado) => {
+
+            Database.all(query, (error, res) => {
                 if (error) {
                     reject(error.message)
                 } else {
-                    resolve(resultado)
+                    resolve(res)
                 }
             })
         })
     }
 
     static listarLimpeza(id, query) {
+
         return new Promise((resolve, reject) => {
-            Database.all(query, id, (error, resultado) => {
+
+            Database.all(query, id, (error, res) => {
                 if (error) {
                     reject(error.message)
                 } else {
-                    resolve(resultado)
+                    resolve(res)
                 }
             })
         })
+    }
+
+    static atualizar(entidade, id, query){
+        const objeto = Object.values(entidade)
+        // const objeto = new LimpezaModel(...Object.values(req.body))
+
+        return new Promise ((resolve, reject) => {
+
+            Database.run(query, [...objeto, id], (error, result) => {
+                if(error){
+                    reject(error.message)
+                }else{
+                    // resolve({erro: false, message: `Registro de número ${id} atualizado`})
+                    resolve(result)
+
+                }
+            })
+        })
+    }
+
+    static deletar(query, id){
+
+        return new Promise((resolve, reject) => {
+            Database.run(query, id, (error) => {
+                if(error){
+                    reject(error.message)
+                }else{
+                    resolve({error: false, message: `Registro ${id} deletado!`})
+                }
+            })
+        })        
     }
 }
 
