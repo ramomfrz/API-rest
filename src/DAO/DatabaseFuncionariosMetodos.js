bb681ef872a2792e2d2ca11b4e2653206273941b
carolinadesouzasilva <carolsouza21@gmail.com>
2022-07-31 01:04:37 -0300

    Corrige declaração errada no arquivo DatabaseFuncionariosMetodos.js na pasta src/DAO

--- src/DAO/DatabaseFuncionariosMetodos.js
+++ src/DAO/DatabaseFuncionariosMetodos.js
@@ -1,11 +1,13 @@
-import Database from "../database/Database.js"
 
-class DatabaseFuncionariosMetodos extends DAO {
+//import Database from "../database/Database.js"
+import funcionariosDAO from "./funcionariosDAO.js"
+
+class DatabaseFuncionariosMetodos extends funcionariosDAO {
     static async createTableFuncionarios(){
 
         const query = `
         CREATE TABLE IF NOT EXISTS funcionarios(
-            id  INTEGER PRIMARY KEY AUTOINCREMENT,
+            id  VARCHAR PRIMARY KEY AUTOINCREMENT,
             nome VARCHAR NOT NULL,
             data_admissao VARCHAR NOT NULL
 
@@ -17,32 +19,31 @@ class DatabaseFuncionariosMetodos extends DAO {
 
     static async insertFuncionarios(funcionario){
         const query = `INSERT INTO funcionarios(id, nome, data_admissao) VALUES (?,?,?)`
-        const response = await this.insertFuncionarios(funcionario, query)
+        const response = await this.insert(funcionario, query)
         return response
     }
 
-    static async listAllFuncionarios(id){
+    static async listAllFuncionarios(){
         const query = `SELECT * FROM funcionarios`
         const response = await this.listAll(query)
         return response
     }//confirmar se está correto
 
-    static async listFunconariosById(id, nome){
+    static async listFunconariosById(id){
         const query = `SELECT * FROM funcionarios WHERE id = ?`
-        const response = await this.listAllFuncionario(id, nome, query)
+        const response = await this.listFuncionario(id, query)
         return response
     }//confirmar se está certo
 
-    static async updateFuncionarios(nome){
-        const query = `UPDATE funcionarios SET nome ?, data_admissao ? WHERE id=?
-        `
-        const response = await this.updateFuncionarios(id, nome, query)
+    static async updateFuncionarios(id){
+        const query = `UPDATE funcionarios SET nome ?, data_admissao ? WHERE id=?`
+        const response = await this.update(id, nome, query)
         return response
     }
 
     static async deleteFuncionario(id){
         const query = `DELETE FROM funconarios WHERE id=?`
-        const response = await this.deleteFuncionario(id, query)
+        const response = await this.delete(id, query)
         return response
     }
 }
