aaa72452f9f821a2f47eca1a75a295ecc4eea878
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-02 19:15:41 -0300

    refactor: Apaga validações duplicadas e adiciona a validação de nome as reservas.

--- src/services/ValidacoesService.js
+++ src/services/ValidacoesService.js
@@ -1,20 +1,8 @@
 export default class ValidacoesService {
 
-    //  VALIDAR NOME    //
-
     static validaNome(nome) {
         return nome.length >= 2
     }
-
-    static ValidarNome(nome){
-        return nome.length >= 3
-    }
-
-    static ValidarNome(nome){
-        return nome.length >= 3
-    }
-
-    //  VALIDAR NUMERO DO QUARTO  //
 
     static validaNumeroQuarto(numeroQuarto) {
         if (numeroQuarto >= 1 && numeroQuarto <= 9999) {
@@ -26,14 +14,10 @@ export default class ValidacoesService {
         }
     }
 
-    //  VALIDAR CPF   //
-
     static validaCPF(CPF) {
         const regexCPF = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/
         return regexCPF.test(CPF)
     }
-
-    //  VALIDAR DATA  //
 
     static validaDataEntrada(dataEntrada) {
         const regexData = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
@@ -45,13 +29,11 @@ export default class ValidacoesService {
         return regexData.test(dataSaida)
     }
 
-    static validaDataAdmissao(data_admissao){
+    static validaDataAdmissao(data_admissao) {
         const regexData = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
-            return regexData.test(data_admissao)
+        return regexData.test(data_admissao)
     }
 
-    //  VALIDAR CONTROLE  //
-    
     /**
      * @param {string} controle
      * @returns boolean
@@ -63,14 +45,12 @@ export default class ValidacoesService {
         return limpou === 1 || limpou === 0
     }
 
-    //  VALIDAR ID_FUNCIONARIO    //
-
     /**
      * @param {string} id_funcionario
      * @returns boolean
      */
 
-    static validaIdFuncionario(id_funcionario){
+    static validaIdFuncionario(id_funcionario) {
         if (id_funcionario >= 1 && id_funcionario < 9999) {
             if (typeof (id_funcionario) != "number") {
                 return false
@@ -80,19 +60,13 @@ export default class ValidacoesService {
         }
     }
 
-    //  VALIDAR TELEFONE  //
-
     static validaTelefone(telefone) {
         const telefoneValidado = parseInt(telefone)
         return telefoneValidado == telefone
     }
 
-
-
-    //  VALIDAÇÕES GERAIS //
-
-    static validarFuncionario(nome, data_admissao){
-        return this.ValidarNome(nome) && this.validaDataAdmissao(data_admissao)
+    static validarFuncionario(nome, data_admissao) {
+        return this.validaNome(nome) && this.validaDataAdmissao(data_admissao)
     }
 
     /**
@@ -104,15 +78,15 @@ export default class ValidacoesService {
      */
 
     static ehValido(numeroQuarto, controle, id_funcionario) {
-        return this.validaNumeroQuarto(numeroQuarto) && this.validaControle(controle) && this.validaIdFuncionario(id_funcionario) 
+        return this.validaNumeroQuarto(numeroQuarto) && this.validaControle(controle) && this.validaIdFuncionario(id_funcionario)
     }
 
-    static reservaValidada(cpfCliente, numeroQuarto, dataEntrada, dataSaida) {
-        return this.validaNumeroQuarto(numeroQuarto) && this.validaCPF(cpfCliente) && this.validaDataEntrada(dataEntrada) && this.validaDataSaida(dataSaida)
+    static reservaValidada(cpfCliente, nome, numeroQuarto, dataEntrada, dataSaida) {
+        return this.validaNumeroQuarto(numeroQuarto) && this.validaCPF(cpfCliente) && this.validaDataEntrada(dataEntrada) && this.validaDataSaida(dataSaida) && this.validaNome(nome)
     }
 
-    static validaGeral(numeroQuarto, nomeHospedes, controle, telefone){
-        return this.validaNumeroQuarto(numeroQuarto) && this.ValidarNome(nomeHospedes) && this.validaControle(controle) && this.validaTelefone(telefone)
+    static validaGeral(numeroQuarto, nomeHospedes, controle, telefone) {
+        return this.validaNumeroQuarto(numeroQuarto) && this.validaNome(nomeHospedes) && this.validaControle(controle) && this.validaTelefone(telefone)
     }
 
     static validaHospede(cpf, nome, numeroQuarto, telefone) {
