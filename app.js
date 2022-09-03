13d684ed2f2b2e69c8758d55478a157f03d51924
allanbarbozag <allpatrick.b@gmail.com>
2022-09-03 12:45:56 -0300

    feat: configura o cors no projeto.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import Limpezas from "./src/controller/limpezaController.js";
 import Reservas from "./src/controller/Reservas.js";
 import Funcionarios from "./src/controller/funcionarios-controller.js";
 import Hospedes from "./src/controller/Hospedes.js";
+import cors from 'cors'
 
 dotenv.config();
 
@@ -17,6 +18,14 @@ app.listen(port, () => {
 })
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    console.log('acessou')
+    app.use(cors());
+    next();
+})
 
 Quartos.rotas(app);
 Limpezas.rotas(app);
