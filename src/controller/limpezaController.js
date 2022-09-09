992437de63698c7c40ac73583065fe5b26c61f63
Ramom <ramomferraz@hotmail.com>
2022-09-08 22:33:03 -0300

    refactor: corrige erro na importação

--- src/controller/limpezaController.js
+++ src/controller/limpezaController.js
@@ -1,10 +1,10 @@
-import LimpezaModel from "../models/LimpezaModel.js";
+import LimpezaModel from "../models/limpezaModel.js";
 import ValidacoesService from "../services/ValidacoesService.js";
 import DatabaseLimpezasMetodos from "../DAO/DatabaseLimpezasMetodos.js";
 
 DatabaseLimpezasMetodos.createTableLimpezas()
-class Limpezas{
-    static rotas(app){       
+class Limpezas {
+    static rotas(app) {
         app.get("/limpezas", async (req, res) => {
 
             const response = await DatabaseLimpezasMetodos.listarTodasLimpezas()
@@ -13,62 +13,62 @@ class Limpezas{
 
         app.get("/limpezas/:id", async (req, res) => {
 
-            try{
+            try {
                 const limpeza = await DatabaseLimpezasMetodos.listarLimpezaPorId(req.params.id)
-                if(limpeza){
-                    res.status(200).json(limpeza)      
-                }else{
+                if (limpeza) {
+                    res.status(200).json(limpeza)
+                } else {
                     throw new Error("Não foi possível encontrar o registro de limpeza")
 
                 }
-            }catch(error){
+            } catch (error) {
                 res.status(404).json(error.message)
             }
         })
-        
+
         app.post("/limpezas", async (req, res) => {
 
             const ehValido = ValidacoesService.ehValido(...Object.values(req.body))
 
-            try{
-                if(ehValido){
+            try {
+                if (ehValido) {
                     const limpeza = new LimpezaModel(...Object.values(req.body))
                     const resposta = await DatabaseLimpezasMetodos.inserirLimpeza(limpeza)
                     res.status(201).json(resposta)
-                }else{
+                } else {
                     throw new Error("Revise o corpo da requisição")
-                }  
-            }catch(error){
+                }
+            } catch (error) {
                 res.status(400).json(error.message)
-            }            
+            }
         })
 
         app.put("/limpezas/:id", async (req, res) => {
             const ehValido = ValidacoesService.ehValido(...Object.values(req.body))
-            try{
-                if(ehValido){
+            try {
+                if (ehValido) {
                     const limpeza = new LimpezaModel(...Object.values(req.body))
                     const resposta = await DatabaseLimpezasMetodos.atualizarLimpezaPorId(req.params.id, limpeza)
                     res.status(200).json(resposta)
-                }else{
+                } else {
                     throw new Error("Erro ao atualizar")
                 }
-            }catch(error){
+            } catch (error) {
                 res.status(400).json(error.message)
             }
         })
 
         app.delete("/limpezas/:id", async (req, res) => {
             //listar por id, se conseguir, deletar
-            try{
+            try {
                 const procuraLimpeza = await DatabaseLimpezasMetodos.listarLimpezaPorId(req.params.id)
-                if(!procuraLimpeza){
+                if (!procuraLimpeza) {
                     throw new Error("Limpeza não encontrada")
-                } else{
+                } else {
                     const deletaLimpeza = await DatabaseLimpezasMetodos.deletarLimpezaPorId(req.params.id)
                     res.status(200).json(deletaLimpeza)
                 }
-            }catch(error){
+            } catch (error) {
                 res.status(404).json(error.message)
             }
         })
